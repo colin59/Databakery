d693052992ce35d7effa6f5a6df91db21b95baa4
Chris <christophe.duruisseau@gmail.com>
2020-11-20 11:33:29 +0100

    second draft, looks like usable version

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # Import libs
 import dash
+import base64
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
@@ -11,6 +12,7 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import NearestNeighbors
+from sklearn.preprocessing import normalize
 from text_content import *
 
 # region style definition
@@ -77,7 +79,7 @@ CONTENT_STYLE = {
 
 # The style for the text
 TEXT_STYLE = {
-    'textAlign': 'center',
+    'textAlign': 'left',
     'color': '#191970'
 }
 # endregion
@@ -89,14 +91,12 @@ df_ultimate_df = pd.read_csv("ultimate_df.csv")
 def get_location(df, dict_d):
     # Filtering the df on price m2
     filtered_df = df[df['Prix du m2 bâti'] <= dict_d['prix_m2']]
-    if filtered_df.shape[0] < 2000:
+    if filtered_df.shape[0] < 5000:
         return('Désolé, il va falloir revoir vos critères prix et/ou superficie')
     else:
-        features_col = [key for key, value in dict_d.items() if ~
-                        np.isnan(value)]
+        features_col = [key for key, value in dict_d.items() if value != 0]
         features_col.remove('prix_m2')
-        list_input = [value for key, value in dict_d.items() if ~
-                      np.isnan(value)]
+        list_input = [value for key, value in dict_d.items() if value != 0]
         # Define X:
         X = filtered_df[features_col]
         # Transform X
@@ -104,15 +104,14 @@ def get_location(df, dict_d):
         X_scaled = scaler.transform(X)
         # Fit NN:
         nbrs = NearestNeighbors(
-            n_neighbors=2000, algorithm='ball_tree').fit(X_scaled)
+            n_neighbors=5000, algorithm='ball_tree').fit(X_scaled)
         distances, indices = nbrs.kneighbors([list_input[:-1]])
-        #nn = nbrs.kneighbors([list_input], 5, return_distance=False)
         return distances, indices
 
 
-dict_dist = {'dist_maternelle': 0, 'dist_primaire': 0, 'dist_college': 0,
-             'dist_lycee': 0, 'dist_hopital': 0,
-             'dist_bus': 0, 'dist_magasins': 0, 'prix_m2': 2001}
+dict_dist = {'dist_maternelle': 0.000001, 'dist_primaire': 0.000001, 'dist_college': 0.000001,
+             'dist_lycee': 0.000001, 'dist_hopital': 0.000001,
+             'dist_bus': 0.000001, 'dist_magasins': 0.000001, 'prix_m2': 2001}
 # def get_location(df, primaire, maternelle, college, lycee, hopital, bus, magasins, prix_m2):
 #   # Filtering the df on price m2
 #   filtered_df = df[df['Prix du m2 bâti'] <= prix_m2]
@@ -137,120 +136,132 @@ dict_dist = {'dist_maternelle': 0, 'dist_primaire': 0, 'dist_college': 0,
 
 # region figure definitions
 test = get_location(df_ultimate_df, dict_dist)
-for i in test[1]:
-    for j in test[0]:
+for i in reversed(test[1]):
+    for j in reversed(test[0]):
         fig = px.scatter_mapbox(
-            df_ultimate_df.iloc[i], lat="lat",  lon="long", color=j, size=j,  size_max=10, zoom=10, opacity=0.3)
-        fig.update_layout(mapbox_style="open-street-map")
+            df_ultimate_df.loc[i], lat="lat",  lon="long", color=j, width=1500,height=700, zoom=10, opacity=0.3)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={'r':1, 't':0, 'l':0, 'b':0})
+fig.update_coloraxes(colorbar_tickmode='array')
+fig.update_coloraxes(colorbar_tickvals=[test[0].min(),test[0].max()])
+fig.update_coloraxes(colorbar_title_text="Compatibilité")
+fig.update_coloraxes(colorbar_ticktext=['100 %','0 %'])
+
 # endregion
 
 
 # region main
-main_title = html.Div(
-    [
-        html.H1('data Bakery', style=TEXT_STYLE),
-    ]
-)
+main_title = dbc.Row(children=[
+    dbc.Col(align='left',width='auto',children=[
+        html.H1('Data Where House (à Lille!)'),html.H6('by the team Data Bakery')]
+        ,md=5),
+        dbc.Col(width='auto',children=[
+        ]
+        ,md=7)
+        ])
 
 interest_title = html.Div(
     html.H5("Vos centres d'intérêt")
 )
 
 
-interest_menu = dbc.Row(children=[
+interest_menu = dbc.Row(align='left',children=[
     dbc.Col(children=[
         html.Div([
             html.Label('École maternelle'),
             dcc.Dropdown(
                 id='ecole_maternelle_dd',
                 options=[
-                 {'label': 'Pas important', 'value': np.nan},
-                 {'label': 'Moins de 500 m', 'value': 500},
-                 {'label': 'Moins de 1 km', 'value': 1000},
-                 {'label': 'Moins de 5 km', 'value': 5000},
-                 {'label': 'Moins de 10 km', 'value': 10000},
+                 {'label': 'Pas important', 'value': 0},
+                 {'label': 'Moins de 500 m', 'value': 500/10000},
+                 {'label': 'Moins de 1 km', 'value': 1000/10000},
+                 {'label': 'Moins de 5 km', 'value': 5000/10000},
+                 {'label': 'Moins de 10 km', 'value': 10000/10000},
                  ],
-                value=np.nan
+                value=0
             ),
             html.Label('École primaire'),
             dcc.Dropdown(
                 id='ecole_primaire_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 5 km', 'value': 5000},
+                    {'label': 'Pas important', 'value': 0/5000},
+                    {'label': 'Moins de 500 m', 'value': 500/5000},
+                    {'label': 'Moins de 1 km', 'value': 1000/5000},
+                    {'label': 'Moins de 5 km', 'value': 5000/5000},
                 ],
-                value=np.nan
+                value=0
             ),
             html.Label('Collège'),
             dcc.Dropdown(
                 id='college_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 5 km', 'value': 5000},
-                    {'label': 'Moins de 10 km', 'value': 10000},
+                    {'label': 'Pas important', 'value': 0/10000},
+                    {'label': 'Moins de 500 m', 'value': 500/10000},
+                    {'label': 'Moins de 1 km', 'value': 1000/10000},
+                    {'label': 'Moins de 5 km', 'value': 5000/10000},
+                    {'label': 'Moins de 10 km', 'value': 10000/10000},
                 ],
-                value=np.nan
+                value=0
             ),
             html.Label('Lycée'),
             dcc.Dropdown(
                 id='lycee_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 5 km', 'value': 5000},
-                    {'label': 'Moins de 10 km', 'value': 10000},
+                    {'label': 'Pas important', 'value': 0/10000},
+                    {'label': 'Moins de 500 m', 'value': 500/10000},
+                    {'label': 'Moins de 1 km', 'value': 1000/10000},
+                    {'label': 'Moins de 5 km', 'value': 5000/10000},
+                    {'label': 'Moins de 10 km', 'value': 10000/10000},
                 ],
-                value=np.nan
+                value=0
             ),
             html.Label('Hopitaux (CHR)'),
             dcc.Dropdown(
                 id='chr_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 5 km', 'value': 5000},
-                    {'label': 'Moins de 10 km', 'value': 10000},
-                    {'label': 'Moins de 10 km', 'value': 20000},
+                    {'label': 'Pas important', 'value': 0/20000},
+                    {'label': 'Moins de 500 m', 'value': 500/20000},
+                    {'label': 'Moins de 1 km', 'value': 1000/20000},
+                    {'label': 'Moins de 5 km', 'value': 5000/20000},
+                    {'label': 'Moins de 10 km', 'value': 10000/20000},
+                    {'label': 'Moins de 10 km', 'value': 20000/20000},
                 ],
-                value=np.nan
+                value=0
             ),
             html.Label('Grande surfaces'),
             dcc.Dropdown(
                 id='gs_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 5 km', 'value': 5000},
-                    {'label': 'Moins de 10 km', 'value': 10000},
+                    {'label': 'Pas important', 'value': 0},
+                    {'label': 'Moins de 500 m', 'value': 500/10000},
+                    {'label': 'Moins de 1 km', 'value': 1000/10000},
+                    {'label': 'Moins de 5 km', 'value': 5000/10000},
+                    {'label': 'Moins de 10 km', 'value': 10000/10000},
                 ],
-                value=np.nan
+                value=0
             ),
             html.Label('Transport en commun'),
             dcc.Dropdown(
                 id='transport_dd',
                 options=[
-                    {'label': 'Pas important', 'value': np.nan},
-                    {'label': 'Moins de 500 m', 'value': 500},
-                    {'label': 'Moins de 1 km', 'value': 1000},
-                    {'label': 'Moins de 2 km', 'value': 2000},
+                    {'label': 'Pas important', 'value': 0/2000},
+                    {'label': 'Moins de 500 m', 'value': 500/2000},
+                    {'label': 'Moins de 1 km', 'value': 1000/2000},
+                    {'label': 'Moins de 2 km', 'value': 2000/2000},
                 ],
-                value=np.nan
+                value=0
             ),
-            html.Label('Budget (prix au m²)'),
+            html.Div('Budget (prix au m²)'),
+           # dbc.Row(justify='left',align='center',children=[
             dcc.Input(
+                style= {'margin-top': 10},
+                size='4',
                 id="budget",
                 type="number",
-                placeholder="Prix au m²"
-            )
+                placeholder="Prix au m²")
+            #)])
         ])
-]),
+],md=2),
 dbc.Col([
         dcc.Graph(id='graph_map',
                   config={
@@ -260,7 +271,7 @@ dbc.Col([
                   ),
         html.Button("Update",id='button_val')
 
-])
+],md=10)
 ])
 
 map_and_insights = dbc.Row(children=[
@@ -285,11 +296,11 @@ map_and_insights = dbc.Row(children=[
     )])
 # endregion
 
-
 # Main content
 content = html.Div(
     [
         main_title,
+        html.Hr(),
         html.Br(),
         interest_title,
         interest_menu,
@@ -300,12 +311,14 @@ content = html.Div(
 )
 # endregion
 
-
 # use bootstrap stylesheet
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 # define page layout, which contains sidebar and content
-app.layout = html.Div([content])
-
+app.layout = html.Div([content],style={
+'background-image': 'url("/assets/back2.jpg")',
+'background-repeat': 'no-repeat',
+'background-position': 'right top',
+'background-size': '150px 100px'})
 # region callbacks
 
 
@@ -374,11 +387,19 @@ def update_var_budget(col_value):
 )
 def update_graph(n_clicks):
     test = get_location(df_ultimate_df, dict_dist)
-    for i in test[1]:
-        for j in test[0]:
+    for i in reversed(test[1]):
+        for j in reversed(test[0]):
             fig = px.scatter_mapbox(
-                df_ultimate_df.iloc[i], lat="lat",  lon="long", color=j, size=j,  size_max=10, zoom=10, opacity=0.3)
-            fig.update_layout(mapbox_style="open-street-map")
+                df_ultimate_df.loc[i], lat="lat",  lon="long",color=j, size_max=10,width=1500,height=700, zoom=10, opacity=0.3)
+    fig.update_layout()
+    fig.update_layout(mapbox_style="open-street-map")
+    fig.update_layout(margin={'r':1, 't':0, 'l':0, 'b':0})
+    fig.update_coloraxes(colorbar_tickmode='array')
+    fig.update_coloraxes(colorbar_tickvals=[0,1])
+    fig.update_coloraxes(colorbar_title_text="Compatibilité")
+    fig.update_coloraxes(colorbar_ticktext=['100 %','0 %'])
+    # fig.update_coloraxes(colorbar_xpad=0)
+    # fig.update_coloraxes(colorbar_x=1)
     return fig
 
 # Validation button
